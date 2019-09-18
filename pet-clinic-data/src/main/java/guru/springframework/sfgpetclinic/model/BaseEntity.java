29c5e24b97bc006a301adb77d692e85fa5197e22
Patrick1ruls <patrickvermillion555@gmail.com>
2019-09-18 12:04:03 -0500

    Commiting using intellij "git" option

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,4 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class BaseEntity {
+import java.io.Serializable;
+
+/**
+ * Ensures that all models that extend this class bus have an id.
+ */
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
