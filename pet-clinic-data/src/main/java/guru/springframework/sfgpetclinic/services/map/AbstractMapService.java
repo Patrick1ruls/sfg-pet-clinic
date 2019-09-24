293aa64097bb776e1f815e608b3154167b1bb624
Patrick1ruls <patrickvermillion555@gmail.com>
2019-09-24 14:26:45 -0500

    Refactored Map Impl to abstract ID generation. Not sure what that does (lol)

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if(object  != null) { // Make sure object is not null
+            if(object.getId()  == null) { // Try to get objects id.
+                object.setId(getNextId()); // But if objects id is null grab the next id (ex: if last id was 3L. Next id would be 4L)
+            }
+            map.put(object.getId(), object);
+        } else {  // If object is null
+            throw new RuntimeException("Object cannot be null"); // Fail gracefully
+        }
 
         return object;
     }
@@ -29,5 +35,17 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+
+        try { // Check if there is an id value in the map
+            nextId = Collections.max(map.keySet()) +  1; // If there is a value in the map, then code will add 1L to it
+        } catch (NoSuchElementException e) {
+            nextId = 1L; // If there is no id value in the map, then initalize with value of 1L
+        }
+
+        return nextId;
     }
 }
