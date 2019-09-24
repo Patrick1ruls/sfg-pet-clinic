99a8c8216e1d70c5239e773427042a0a7f35bdcc
Patrick1ruls <patrickvermillion555@gmail.com>
2019-09-24 12:12:57 -0500

    Adding owner list to updated controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners") // Removes the need for specifying /owners in the request mapping below
 @Controller // The traffic controller for the owners web page
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll()); // Produces an iterative set of owners called owners
 
         return "owners/index"; // hits the thymeleaf ownerss.index.html page
     }
